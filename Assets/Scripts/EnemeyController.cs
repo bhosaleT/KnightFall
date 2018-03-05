5740aa921ce7b29a9b4e81e90730b8f80d4e3e68
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-03-05 21:27:27 +0530

    Enemies Added

---
+++ Assets/Scripts/EnemeyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemeyController : MonoBehaviour {
+
+    // Use this for initialization
+    public float moveSpeed;
+    private bool canMove;
+
+    private Rigidbody2D myRigidBody;
+
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (canMove)
+        {
+            myRigidBody.velocity = new Vector3(-moveSpeed, myRigidBody.velocity.y, 0f);
+        }
+	}
+
+    void OnBecameInvisible()
+    {
+        canMove = true;
+    }
+}
