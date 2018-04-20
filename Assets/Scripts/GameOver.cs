d890588652881d2156735d4142b0cb118eef8701
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-04-20 18:58:42 +0530

    Working Pause menu

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public string levelSelect;
+    public string mainMenu;
+    private LevelManager theLevelManager;
+
+	// Use this for initialization
+	void Start () {
+        theLevelManager = FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Restart()
+    {
+        PlayerPrefs.SetInt("CoinCount", 0);
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.startingLives);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LevelSelect()
+    {
+        PlayerPrefs.SetInt("CoinCount", 0);
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.startingLives);
+
+        SceneManager.LoadScene(levelSelect);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+
+    public string levelSelect;
+    public string mainMenu;
+    private LevelManager theLevelManager;
+    public GameObject thePauseScreen;
+    private PlayerController thePlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        theLevelManager = FindObjectOfType<LevelManager>();
+        thePlayer = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (Time.timeScale == 0f)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        thePlayer.canMove = false;
+        Time.timeScale = 0f;
+        theLevelManager.levelMusic.Pause();
+        thePauseScreen.SetActive(true);
+    }
+
+    public void ResumeGame()
+
+    {
+        thePlayer.canMove = true;
+        Time.timeScale = 1f;
+        theLevelManager.levelMusic.Play();
+        thePauseScreen.SetActive(false);
+    }
+
+    public void LevelSelect()
+    {
+        Time.timeScale = 1f;
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.startingLives);
+        PlayerPrefs.SetInt("CoinCount", 0);
+
+
+
+        SceneManager.LoadScene(levelSelect);
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+}
